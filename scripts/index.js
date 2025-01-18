a0bb853f01af06555a17b89a094963d88deb2fbd
Bogdan <bodyarovoy@gmail.com>
2025-01-19 00:16:40 +0400

    v1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardContent) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = cardContent.link;
+    cardElement.querySelector('.card__image').alt = cardContent.name;
+    cardElement.querySelector('.card__title').textContent = cardContent.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+    return cardElement;
+    }
 
 // @todo: Функция удаления карточки
-
+function deleteCard(card) {
+      card.remove();
+    }
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardInfo) => {
+    const card = createCard(cardInfo);
+    placesList.append(card);
+});
