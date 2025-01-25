48367a8aeb661958bf954e347371ed8b6a370784
Bogdan <bodyarovoy@gmail.com>
2025-01-25 23:13:45 +0400

    v2.0

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,25 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(cardContent) {
+function createCard(cardContent, deleteCardCallback) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = cardContent.link;
-    cardElement.querySelector('.card__image').alt = cardContent.name;
+    const cardImageAttribute = cardElement.querySelector('.card__image');
+    cardImageAttribute.src = cardContent.link;
+    cardImageAttribute.alt = cardContent.name;
     cardElement.querySelector('.card__title').textContent = cardContent.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
-    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+    deleteButton.addEventListener('click', () => deleteCardCallback(cardElement))
 
     return cardElement;
     }
 
 // @todo: Функция удаления карточки
-function deleteCard(card) {
+function deleteCardCallback(card) {
       card.remove();
     }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardInfo) => {
-    const card = createCard(cardInfo);
+    const card = createCard(cardInfo, deleteCardCallback);
     placesList.append(card);
 });
