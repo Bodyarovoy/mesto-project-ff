468387f6ee4dc58ea076bf510e68c089a2b3705a
Bogdan <bodyarovoy@gmail.com>
2025-02-27 13:21:21 +0400

    v3.0

--- scripts/components/modal.js
+++ scripts/components/modal.js
@@ -1,55 +1,36 @@
 export { openPopup, closePopup };
 
-
 // Функция открытия попапов
 function openPopup(popup) {
+
     popup.classList.add('popup_is-opened');
     document.addEventListener('keydown', closeByEsc);
     popup.addEventListener('click', closeByClick);
-}
+  } 
 
 // Функция закрытия попапов
 function closePopup(popup) {
+  if (popup && popup.classList && popup.classList.contains('popup_is-opened')) { 
     popup.classList.remove('popup_is-opened');
     document.removeEventListener('keydown', closeByEsc);
     popup.removeEventListener('click', closeByClick);
-  
+  } 
 }
 
 // Обработчик закрытия по Escape
 function closeByEsc(event) {
   if (event.key === 'Escape') {
-      const openPopup = document.querySelector('.popup_is-opened');
+    const openPopup = document.querySelector('.popup_is-opened');
+    if (openPopup) {
       closePopup(openPopup);
-    }
-
+    } 
+  }
 }
 
 // Обработчик закрытия по клику на оверлей или кнопку закрытия
 function closeByClick(event) {
-   const activePopup = event.currentTarget;
-   
-if (event.target === activePopup || event.target.classList.contains('popup__close')) { 
-      const openPopup = document.querySelector('.popup_is-opened');
-      closePopup(openPopup);
+  const activePopup = event.currentTarget;
+  if (event.target === activePopup || event.target.classList.contains('popup__close')) {
+    closePopup(activePopup);
   }
 }
-
-// function closePopup(popup) {
-//   const activePopup = popup.currentTarget;
-//   const openPopup = document.querySelector('.popup_is-opened');
-
-//   // Закрытие по клику на оверлей или по кнопке закрытия
-//   if (popup.target === activePopup || popup.target.classList.contains('popup__close')) {
-//     openPopup.classList.remove('popup_is-opened');
-//     removeEventListeners();
-//   }
-//   // Закрытие по нажатию Escape
-//   if (popup.key === 'Escape') {  
-//     if (openPopup) {
-//       openPopup.classList.remove('popup_is-opened');
-//       removeEventListeners();
-//     }
-//   }
-// }
-
